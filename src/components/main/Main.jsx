af08f71947a8e7dcfd10d60c76d70a6223e3039f
Michael Swasnon <thor.0688@gmail.com>
2018-08-12 10:17:59 -0500

    fix redirect

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -7,7 +7,7 @@ import { Switch, Route } from 'react-router-dom';
 
 export const Main = (props) => {
   let style = {};
-
+  console.log(props);
   if (window.location.href.endsWith('resume') && window.screen.width < 600) {
     style.overflow = 'auto';
     style.height = '325vh';
@@ -21,6 +21,7 @@ export const Main = (props) => {
         <Route exact path='/resume' component={Resume} />
         <Route exact path='/references' component={References} />
         <Route exact path='/portfolio' component={Portfolio} />
+        <Route component={Home} />
       </Switch>
     </main>
   );
